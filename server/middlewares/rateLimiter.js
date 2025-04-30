c13bcdc3e61e6e3124b0875daf8852ed271f32fa
Dom Nieban <ydnban@gmail.com>
2025-04-30 10:55:15 -0400

    rateLimiter updates -> 10

--- server/middlewares/rateLimiter.js
+++ server/middlewares/rateLimiter.js
@@ -2,7 +2,7 @@ const rateLimit = require("express-rate-limit");
 
 exports.rateLimiter = rateLimit({
     windowMs: 60 * 1000, // 1 minute time window. (MS = miliseconds)
-    max: 4,
+    max: 10,
     // 'Too many request attempts, Try again later'
     handler: (req, res, next)=> {
         let err = new Error('Too many requests. Try again later');
